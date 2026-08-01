22222da0fb8c3f1fd20d597761ab63830e58eeef
wingedcatgirl <69098783+wingedcatgirl@users.noreply.github.com>
2026-07-31 20:19:50 -0400

    oh this isn't needed anymore, huh

--- jokers/000minty.lua
+++ jokers/000minty.lua
@@ -10,18 +10,11 @@ SMODS.Joker{
     loc_txt = {
         name = "Minty :3",
         text = {
-            {
-                "Hi! I made this!",
-                "Go click 'unlock all'",
-                "in the profile menu",
-                "if you want to see",
-                "everything right away!"
-            },
-            {
-                "If the button is gone,",
-                "I've got another mod",
-                "to handle that!"
-            }
+            "Hi! I made this!",
+            "Go click 'unlock all'",
+            "in the config menu",
+            "if you want to see",
+            "everything right away!"
         }
     },
     atlas = "minty_title",
@@ -30,20 +23,6 @@ SMODS.Joker{
     soul_pos = {x=1,y=0},
     in_pool = function (self, args)
         return false
-    end,
-    set_ability = function (self, card, initial, delay_sprites)
-            G.E_MANAGER:add_event(Event {
-                func = function()
-                    if not card.area then return false end
-                    if card.area.config.collection then
-                        function card:click()
-                            love.system.openURL("https://github.com/wingedcatgirl/re-Unlock-All")
-                            card.click = Card.click
-                        end
-                    end
-                    return true
-                end, blocking = false, trigger = "after", delay = 0.25, timer = "UPTIME"
-            })
     end,
     calculate = function (self, card, context)
         if not G.SETTINGS.paused and not (card.area and card.area.config.collection or card.area == G.title_top) and not card.byebye then
